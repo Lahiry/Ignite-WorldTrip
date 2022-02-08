e8f50b96a78f42454ca1756c84333b091ec35c69
Lahiry <raphaellcmb@al.insper.edu.br>
2022-02-08 17:59:38 -0300

    Componente Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  
+
   return (
-    <h1>Hello World!</h1>
+    <Flex h="sm" px="56" justify="space-between" align="center" bgImage="images/background.svg" bgSize="cover">
+      <Box>
+        <Heading lineHeight="auto" color="gray.50" fontSize="4xl" fontWeight="500">
+          5 Continentes,<br />infinitas possibilidades.
+        </Heading>
+        <Text mt="5" color="gray.100" fontSize="xl" >Chegou a hora de tirar do papel a viagem que você<br />sempre sonhou.</Text>
+      </Box>
+      <Image src="images/airplane.svg" alt="avião" w="lg" mt="32" />
+    </Flex>
   )
 }
